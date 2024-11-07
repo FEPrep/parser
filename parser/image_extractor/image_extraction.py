e3dd5da592e624f8a994aa08c6fb740633655527
e4s70f3d3n <avitulli@knights.ucf.edu>
2024-11-07 18:31:52 +0000

    perf: :construction: Begin conversion for the Image class Pydantic model

--- parser/image_extractor/image_extraction.py
+++ parser/image_extractor/image_extraction.py
@@ -2,14 +2,19 @@ import io
 import os
 
 import fitz
-from PIL import Image
+
+# from PIL import Image
+from Typing import List
+from metadata_extraction import extract_metadata
+from models import Image
 
 
-def extract_images(pdf_path, output_dir):
+def extract_images(pdf_path, output_dir) -> List[Image]:
     pdf_name = os.path.splitext(os.path.basename(pdf_path))[0]
     output_dir = os.path.join(output_dir, pdf_name)
     os.makedirs(output_dir, exist_ok=True)
     pdf_document = fitz.open(pdf_path)
+    metadata_list = []
 
     for page_number in range(len(pdf_document)):
         page = pdf_document[page_number]
@@ -32,5 +37,9 @@ def extract_images(pdf_path, output_dir):
             )
             image.save(image_path)
 
+            metadata = extract_metadata(img_index, xref, page, image_ext, image_path)
+            metadata_list.append(metadata)
+
     pdf_document.close()
     print(f"Images successfully extracted and saved to {output_dir}.")
+    return metadata_list
