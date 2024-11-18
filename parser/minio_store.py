ac4ea426ecb81d596eac87cc671f1dc3eb05e4da
anna-sage-local <anna.macinnis109@gmail.com>
2024-11-18 03:46:56 +0000

    refactor(parser): :art: better error handling
    error handling matches standard approach

--- parser/minio_store.py
+++ parser/minio_store.py
@@ -3,13 +3,14 @@
 import pymupdf
 import io
 import os
+import sys
 from dotenv import load_dotenv
 from minio import Minio
 from parser.dataset.exam import Exam
 
 
 class Bucket:
-    client: Minio | None = None
+    client: Minio
     bucket_name: str = "fe-pdfs"
 
     def __init__(self):
@@ -28,7 +29,9 @@ class Bucket:
     def create_exam_objs(self, exam: Exam):
         document = pymupdf.open(exam.exam_path)
 
+        assert exam.sections
         for section in exam.sections:
+            assert section.questions
             for question in section.questions:
                 # Create a new empty PDF named with a unique ID
                 pdf_dir = f"{exam.semester}-{exam.year}"
@@ -43,15 +46,11 @@ class Bucket:
                     )
                     question_pdf_page.show_pdf_page(rect, document, page)
 
-                # Save the pdf locally
-                if not os.path.exists(pdf_dir):
-                    os.makedirs(pdf_dir)
-                question_pdf.save(question_pdf_name)
-
                 pdf_bytes = question_pdf.write()
                 pdf_stream = io.BytesIO(pdf_bytes)
 
                 try:
+                    # Add the file to the bucket
                     self.client.put_object(
                         bucket_name=self.bucket_name,
                         object_name=question_pdf_name,
@@ -60,5 +59,8 @@ class Bucket:
                         content_type="application/pdf",
                     )
                 except Exception as e:
-                    # todo make this better lol
-                    print(f"uh oh!\n{e}")
+                    print(f"An error occurred: {e}", file=sys.stderr)
+                    import traceback
+
+                    traceback.print_exc()
+                    sys.exit(1)
