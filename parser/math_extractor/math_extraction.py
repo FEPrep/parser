4050c467cc95edd179ccaff07f0c25e19ce57ff2
anna-sage-local <anna.macinnis109@gmail.com>
2024-11-19 09:13:58 +0000

    feat(experiments): :monocle_face: detecting summations and upper/lower bounds
    detecting and printing a math span's designation as summation, upper bound, lower bound, or none of the above

--- parser/math_extractor/math_extraction.py
+++ parser/math_extractor/math_extraction.py
@@ -2,6 +2,7 @@ import os
 import fitz
 
 
+# Extract all math text into json containing necessary metadata / context
 def extract_math_text(pdf_path, output_dir):
     pdf_name = os.path.splitext(os.path.basename(pdf_path))[0]
     output_dir = os.path.join(output_dir, pdf_name)
@@ -12,12 +13,42 @@ def extract_math_text(pdf_path, output_dir):
         page = pdf_document[page_num]
         text_blocks = page.get_text("dict")["blocks"]
 
-        for block in text_blocks:
-            # Only process text blocks (type 0)
-            if block["type"] == 0:
-                for line in block["lines"]:
-                    for span in line["spans"]:
-                        if span["font"] == "CambriaMath":
-                            print(
-                                f"{span["text"]}\non page {page_num} with bbox {span["bbox"]}"
-                            )
+        math_spans = get_math_span_list(text_blocks)
+
+        bounds_to_process = 0
+        for span in math_spans:
+            print("===========================")
+            if bounds_to_process > 0:
+                if bounds_to_process % 2 == 0:
+                    print("type: upper bound")
+                else:
+                    print("type: lower bound")
+
+                bounds_to_process -= 1
+            else:
+                bounds_to_process = 2 * amt_summations_in_span(span)
+                if bounds_to_process > 0:
+                    print("type: summation")
+                else:
+                    print("type: not summation")
+
+            print(f"value: {span["text"]}")
+            print("===========================")
+
+
+# Returns a list of spans containing math text
+def get_math_span_list(text_blocks):
+    result = []
+    for block in text_blocks:
+        # Only process text blocks
+        if block["type"] == 0:
+            for line in block["lines"]:
+                for span in line["spans"]:
+                    if span["font"] == "CambriaMath":
+                        result.append(span)
+
+    return result
+
+
+def amt_summations_in_span(span):
+    return span["text"].count("\u2211")
